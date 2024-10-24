9171d6e00d752c37b196c7aab2dd362b921d9eac
Eduardo Cruz <eduardofrankcruzmendez@gmail.com>
2024-10-24 15:00:50 -0300

    configuraciones

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        projectId:"tp-final-labo4-eduardo-cruz",
+        appId:"1:335867561110:web:9d227035959876b77c6eb7",
+        storageBucket:"tp-final-labo4-eduardo-cruz.appspot.com",
+        apiKey:"AIzaSyAqAYk5TO0AEO1t1lrwTZK9OmhR147PYoc",
+        authDomain:"tp-final-labo4-eduardo-cruz.firebaseapp.com",
+        messagingSenderId:"335867561110"
+      }
+};
